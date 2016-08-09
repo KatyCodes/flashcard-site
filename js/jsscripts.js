46b131ee5f2c00f08e79f63d89ece9b6639bc18d
Jena SanCartier and Katy Henning <katy@>
2016-08-09 15:56:59 -0700

    adds styling to flashcards as well as slide toggle

--- js/jsscripts.js
+++ js/jsscripts.js
@@ -1,5 +1,12 @@
+// $(document).ready(function() {
+//   $(".col-sm-3").click(function() {
+//     $(this).children().toggle();
+//   });
+// });
+
 $(document).ready(function() {
   $(".col-sm-3").click(function() {
-    $(this).children().toggle();
+    $(this).children().slideToggle();
+    $(this).slideToggle().stop();
   });
 });
